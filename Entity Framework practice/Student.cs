8bed1c742a2fcb0589a5ea433dfdaaa3d2606cf4
pwillsmaith <pwillsmaith@gmail.com>
2022-05-28 13:39:10 -0400

    initial commit

---
+++ Entity Framework practice/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity_Framework_practice
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirestName { get; set; }
+        public int LastName { get; set; }
+    }
+}
